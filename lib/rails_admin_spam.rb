f5deb61a4389cad59077900016b2a3ca43586f31
Eric Hochberger <eric@mediavine.com>
2012-02-07 15:23:41 -0500

    Removed spam translations... since theyre ignored, fixed object => member, added unspam

--- lib/rails_admin_spam.rb
+++ lib/rails_admin_spam.rb
@@ -1,40 +1,69 @@
-require "rails_admin_spam/engine"
-
-module RailsAdminSpam
-end
-
 require 'rails_admin/config/actions'
+require 'rails_admin/config/actions/base'
 
 module RailsAdmin
   module Config
     module Actions
-      class Spam < Base
+      class Spam < RailsAdmin::Config::Actions::Base
         RailsAdmin::Config::Actions.register(self)
 
-        register_instance_option :object_level do
+        register_instance_option :member do
           true
         end
 
         register_instance_option :bulkable? do        
           true
         end
-                
+
+        register_instance_option :link_icon do
+          'icon-ban-circle'
+        end
+              
         register_instance_option :controller do
           Proc.new do
-            
             if params[:bulk_ids]
-              params[:bulk_ids].each {|id| Comment.find(id).spam }
-              flash[:success] = "#{params[:bulk_ids].size} Comments Spammed"
+              params[:bulk_ids].each {|id| @abstract_model.find(id).spam }
+              flash[:success] = "#{params[:bulk_ids].size} Spammed"
             else
               @object.spam
-              flash[:success] = "Single spammed."
+              flash[:success] = "Deleted and Marked as spam."
             end
             
             redirect_to back_or_index
           end
         end
-                
       end
+
+      class Unspam < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+
+        register_instance_option :member do
+          true
+        end
+
+        register_instance_option :bulkable? do        
+          true
+        end
+
+        register_instance_option :link_icon do
+          'icon-ok'
+        end
+
+        register_instance_option :controller do
+          Proc.new do
+            if params[:bulk_ids]
+              params[:bulk_ids].each {|id| @abstract_model.find(id).unspam }
+              flash[:success] = "#{params[:bulk_ids].size} Unspammed"
+            else
+              @object.unspam
+              flash[:success] = "Comment restored and Akismet notified."
+            end
+
+            redirect_to back_or_index
+          end
+        end    
+      end
+      
     end
   end
 end
--- lib/rails_admin_spam/version.rb
+++ lib/rails_admin_spam/version.rb
@@ -1,3 +1,3 @@
 module RailsAdminSpam
-  VERSION = "0.0.3"
+  VERSION = "0.0.6"
 end
